91ce207d0fa86a5c6d2f70e6f8b0d57851927264
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2021-07-22 16:42:04 +0500

    Add style

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,18 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
